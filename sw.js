3654cf0d81fb915a4eeae7feca9b9a2f081a6c0f
Cuite Piglin <65344456+cmy2008@users.noreply.github.com>
2025-07-14 12:58:24 +0800

    Site updated: 2025-07-14 12:58:23

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1752468591508";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1752469051999";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
