b9691d0803f90cea68407e9644e9e94e0a3afd87
Cuite Piglin <cmy2008@foxmail.com>
2026-05-05 04:19:11 +0800

    Site updated: 2026-05-05 04:19:11

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1776340490203";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1777925839054";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
