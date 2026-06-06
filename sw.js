b82b38e2ebd2277ecae375b7c2260cf268d55cbe
cmy2008 <cmy2008@foxmail.com>
2026-06-06 19:30:37 +0800

    Site updated: 2026-06-06 19:30:36

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1777925839054";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1780745184547";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
